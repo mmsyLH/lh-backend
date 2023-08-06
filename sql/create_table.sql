070866547615598fbec7016f1f1d1cbccc0b4064
yupi <592789970@qq.com>
2023-08-06 19:20:41 +0800

    问题修复：完善 create_table.sql 建表语句

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,6 +1,13 @@
 # 数据库初始化
 # @author <a href="https://github.com/liyupi">程序员鱼皮</a>
 # @from <a href="https://yupi.icu">编程导航知识星球</a>
+
+-- 创建库
+create database if not exists yupi;
+
+-- 切换库
+use yupi;
+
 # 用户表
 create table user
 (
@@ -21,5 +28,8 @@ create table user
     planetCode   varchar(512)                       null comment '星球编号'
 )
     comment '用户';
+
+# 导入示例用户
+INSERT INTO yupi.user (username, userAccount, avatarUrl, gender, userPassword, phone, email, userStatus, createTime, updateTime, isDelete, userRole, planetCode) VALUES ('鱼皮', 'yupi', 'https://himg.bdimg.com/sys/portraitn/item/public.1.e137c1ac.yS1WqOXfSWEasOYJ2-0pvQ', null, 'b0dd3697a192885d7c055db46155b26a', null, null, 0, '2023-08-06 14:14:22', '2023-08-06 14:39:37', 0, 1, '1');
 
 # [加入编程导航](https://t.zsxq.com/0emozsIJh) 入门捷径+交流答疑+项目实战+求职指导，帮你自学编程不走弯路
